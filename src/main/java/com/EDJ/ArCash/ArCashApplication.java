285f0acda5c77d778a1ff79f8ff43ba008e97386
INizetich <ignacionizetich@gmail.com>
2025-04-29 12:29:43 -0300

    Comentario sobre railway

--- src/main/java/com/EDJ/ArCash/ArCashApplication.java
+++ src/main/java/com/EDJ/ArCash/ArCashApplication.java
@@ -3,6 +3,9 @@ package com.EDJ.ArCash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/// ES POSIBLE BUILDEAR LA APP EN RAILWAY SIN DOCKER FILE. SOLO JAVA 17.
+/// https://arcash.ddns.net/
+
 @SpringBootApplication
 public class ArCashApplication {
 
