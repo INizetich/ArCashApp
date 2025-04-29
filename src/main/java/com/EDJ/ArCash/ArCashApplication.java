184fd45f5246876c63593a7353f7eddb8752bf72
INizetich <ignacionizetich@gmail.com>
2025-04-29 12:07:11 -0300

    first commit

---
+++ src/main/java/com/EDJ/ArCash/ArCashApplication.java
@@ -0,0 +1,13 @@
+package com.EDJ.ArCash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArCashApplication.class, args);
+	}
+
+}
