3cddf6e3a10b0a594daedab7a0f2f108968c14c3
INizetich <ignacionizetich@gmail.com>
2025-04-29 12:16:27 -0300

    Puerto agregado para Railway

--- src/main/java/com/EDJ/ArCash/Controller/DemoController.java
+++ src/main/java/com/EDJ/ArCash/Controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DemoController {
 
-    @RequestMapping("/home")
+    @RequestMapping({"/home", "/"})
     public String home(){
         return "demo";
     }
