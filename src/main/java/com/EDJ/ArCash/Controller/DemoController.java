1ce85344e2325cb8eef320ebc5ee5fcf3fc9a9be
Rufinofg2 <rufinofigueroa513@gmail.com>
2025-05-01 19:36:03 -0300

    actualizacion rama frontend

--- src/main/java/com/EDJ/ArCash/Controller/DemoController.java
+++ src/main/java/com/EDJ/ArCash/Controller/DemoController.java
@@ -2,6 +2,8 @@ package com.EDJ.ArCash.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import com.EDJ.ArCash.Models.User;
 
 @Controller
 public class DemoController {
