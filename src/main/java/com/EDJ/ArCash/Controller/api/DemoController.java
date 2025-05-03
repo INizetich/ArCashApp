52122572c3f67be230a65cc03cd189d5936cdb01
Ignacio Nizetich <ignacionizetich@gmail.com>
2025-05-03 14:59:08 -0300

    Hotfix v0.0.3: agregue el endpoint por defecto home

--- src/main/java/com/EDJ/ArCash/Controller/api/DemoController.java
+++ src/main/java/com/EDJ/ArCash/Controller/api/DemoController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DemoController {
 
+    @RequestMapping({"/home", "/"})
+    public String home(){
+        return "demo";
+    }
+
     @RequestMapping("/demo")
     public String demo(){
         return "demo";
