16b0e243d24f3f0ea405edb468068a5e0bf14e3e
valerotomas <tomasvalero998@gmail.com>
2025-05-07 05:06:56 -0300

    Ahora se envía un email al usuario cuando se registra. (falta la lógica de validación y agregarlo al email)

--- src/main/java/com/EDJ/ArCash/Controller/api/UserController.java
+++ src/main/java/com/EDJ/ArCash/Controller/api/UserController.java
@@ -3,9 +3,12 @@ package com.EDJ.ArCash.Controller.api;
 import com.EDJ.ArCash.DTO.RegisterResponse;
 import com.EDJ.ArCash.Models.User;
 import com.EDJ.ArCash.Service.UserService;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
 
 @RestController
 public class UserController {
@@ -18,7 +21,7 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<RegisterResponse> register(@RequestBody User user){
+    public ResponseEntity<RegisterResponse> register(@RequestBody User user) throws MessagingException, UnsupportedEncodingException {
         userService.insertarUsuario(user);
         return ResponseEntity.ok(new RegisterResponse(true,"Usuario registrado correctamente"));
     }
---
+++ src/main/java/com/EDJ/ArCash/Service/EmailService.java
@@ -0,0 +1,45 @@
+package com.EDJ.ArCash.Service;
+
+import com.EDJ.ArCash.Models.User;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import java.io.UnsupportedEncodingException;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    public EmailService(JavaMailSender mailSender, SpringTemplateEngine templateEngine) {
+        this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
+    }
+
+    public void testEmail(User user) throws MessagingException, UnsupportedEncodingException {
+        Context context = new Context();
+        context.setVariable("username", user.getName());
+
+        String html = templateEngine.process("email", context);
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, StandardCharsets.UTF_8.name());
+
+        // Establecer el nombre del remitente y la dirección de correo
+        helper.setFrom("helparcash2025@gmail.com", "ArCash");
+
+        helper.setTo(user.getEmail());
+        helper.setSubject("¡Bienvenido a ArCashApp, " + user.getName() + "!");
+
+        // Asegurar que el contenido sea HTML y usar UTF-8 para evitar problemas de codificación
+        helper.setText(html, true);
+
+        mailSender.send(mimeMessage);
+    }
+}
