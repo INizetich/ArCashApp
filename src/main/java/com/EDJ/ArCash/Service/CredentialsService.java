7688360461dd673c6e2121b734e401cd2f3ebeed
Tomás Valero <128714032+ValeroTomas@users.noreply.github.com>
2025-05-07 05:15:45 -0300

    Actualización de Emails (#6)
    * Eliminadas validaciones de zoho
    
    * Ahora se envía un email al usuario cuando se registra. (falta la lógica de validación y agregarlo al email)

--- src/main/java/com/EDJ/ArCash/Service/CredentialsService.java
+++ src/main/java/com/EDJ/ArCash/Service/CredentialsService.java
@@ -20,12 +20,8 @@ public class CredentialsService {
 
     public Credentials createCredentials(User user){
         Credentials credentials = new Credentials();
-
-        credentials.setUsername(user.getAlias());
-
-        String userPass = user.getDni();
-        String passEncoded = passwordEncoder.encode(userPass);
-        System.out.println("PASS: " + passEncoded);
+        credentials.setUsername(generateUniqueNickname(user));
+        String passEncoded = passwordEncoder.encode(user.getDni());
         credentials.setPass(passEncoded);
         credentials.setUser(user);
         credentials.setPermissions("USER");
@@ -33,5 +29,11 @@ public class CredentialsService {
     }
 
 
-
+    /// METODOS PRIVADOS PARA LA GENERACIÓN DE UN NICKNAME AUTOMATICO
+    private String generateUniqueNickname(User user) {
+        String nickname = user.getName().substring(0, 1).toUpperCase() +
+                user.getLastName().substring(0, 1).toUpperCase() +
+                user.getDni();
+        return nickname.length() > 25 ? nickname.substring(0, 25).toUpperCase() : nickname.toUpperCase();
+    }
 }
--- src/main/java/com/EDJ/ArCash/Service/UserService.java
+++ src/main/java/com/EDJ/ArCash/Service/UserService.java
@@ -2,7 +2,10 @@ package com.EDJ.ArCash.Service;
 
 import com.EDJ.ArCash.Models.User;
 import com.EDJ.ArCash.Repository.UserRepository;
+import jakarta.mail.MessagingException;
 import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class UserService {
@@ -13,19 +16,23 @@ public class UserService {
 
     private final CredentialsService credentialsService;
 
-    public UserService(UserRepository userRepository, AccountService accountService, CredentialsService credentialsService) {
+    private final EmailService emailService;
+
+    public UserService(UserRepository userRepository, AccountService accountService, CredentialsService credentialsService, EmailService emailService) {
         this.userRepository = userRepository;
         this.accountService = accountService;
         this.credentialsService = credentialsService;
+        this.emailService = emailService;
     }
 
 
-    public void insertarUsuario(User user){
+    public void insertarUsuario(User user) throws MessagingException, UnsupportedEncodingException {
         user.setName(user.getName().substring(0,1).toUpperCase() + user.getName().substring(1).toLowerCase());
         user.setLastName(user.getLastName().substring(0,1).toUpperCase() + user.getLastName().substring(1).toLowerCase());
         userRepository.save(user);
         credentialsService.createCredentials(user);
         accountService.createAccount(user, "PESOS");
+        emailService.testEmail(user);
     }
 
 
