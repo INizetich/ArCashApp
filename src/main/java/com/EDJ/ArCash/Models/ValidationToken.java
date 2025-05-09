da8efc2436fdc895a29703cdf9f6a92e53d8341f
Ignacio Nizetich <68255407+INizetich@users.noreply.github.com>
2025-05-09 02:22:58 -0300

    actualizacion README.md a uno mas completo
    falta detallar y seguir documentando, lo ideal seria tenerlo en español e ingles

--- src/main/java/com/EDJ/ArCash/Models/ValidationToken.java
+++ src/main/java/com/EDJ/ArCash/Models/ValidationToken.java
@@ -1,11 +1,9 @@
 package com.EDJ.ArCash.Models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
